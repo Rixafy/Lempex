583a137dbf1c5423ca0aae1529f4a1a01a54d47c
Rick Strafy <rixafy@gmail.com>
2019-04-12 11:09:55 +0200

    Add Project entity

---
+++ app/Model/Project/Project.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lempex\Model\Project;
+
+use Rixafy\DoctrineTraits\DateTimeTrait;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Table(name="project")
+ */
+class Project
+{
+	/**
+	 * @ORM\Column(type="string", length=127)
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * @ORM\Column(type="text", length=1023)
+	 * @var string
+	 */
+	private $description;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var int
+	 */
+	private $user_uid;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var int
+	 */
+	private $user_gid;
+
+	use DateTimeTrait;
+
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+	public function getDescription(): string
+	{
+		return $this->description;
+	}
+
+	public function getUserUid(): int
+	{
+		return $this->user_uid;
+	}
+
+	public function getUserGid(): int
+	{
+		return $this->user_gid;
+	}
+}
