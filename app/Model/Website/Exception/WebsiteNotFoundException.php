9a800636b1887214af635a4b10149b7c9af1aa3f
Rixafy <rixafy@gmail.com>
2019-04-13 23:08:01 +0200

    Add WebsiteRepository

---
+++ app/Model/Website/Exception/WebsiteNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lempex\Model\Website\Exception;
+
+use Exception;
+
+class WebsiteNotFoundException extends Exception
+{
+
+}
---
+++ app/Model/Website/WebsiteRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lempex\Model\Website;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Lempex\Model\Website\Exception\WebsiteNotFoundException;
+use Ramsey\Uuid\UuidInterface;
+
+class WebsiteRepository
+{
+	/** @var EntityManagerInterface */
+	private $entityManager;
+
+	public function __construct(EntityManagerInterface $entityManager)
+	{
+		$this->entityManager = $entityManager;
+	}
+
+	private function getRepository()
+	{
+		return $this->entityManager->getRepository(Website::class);
+	}
+
+	/**
+	 * @throws WebsiteNotFoundException
+	 */
+	public function get(UuidInterface $id): Website
+	{
+		/** @var Website $website */
+		$website = $this->getRepository()->find($id);
+
+		if ($website === null) {
+			throw new WebsiteNotFoundException('Website with id ' . $id . ' not found.');
+		}
+
+		return $website;
+	}
+
+	/**
+	 * @return Website[]
+	 */
+	public function getAllByProject(UuidInterface $projectId): array
+	{
+		return $this->getRepository()->findBy([
+			'project' => $projectId
+		]);
+	}
+
+	/**
+	 * @return Website[]
+	 */
+	public function getAll(): array
+	{
+		return $this->getRepository()->findAll();
+	}
+}
